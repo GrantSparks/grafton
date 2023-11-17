0383150efeed011fbefe083ceb57d4beb05654c9
Grant Sparks <grant@sparks.to>
2023-11-17 17:57:20 +1100

    SQL formatter for vscode

--- grafton-server/migrations/20231108213118_init.sql
+++ grafton-server/migrations/20231108213118_init.sql
@@ -1,7 +1,6 @@
 -- Create users table.
-create table if not exists users
-(
-    id           integer primary key autoincrement,
-    username     text not null unique,
+create table if not exists users (
+    id integer primary key autoincrement,
+    username text not null unique,
     access_token text not null
 );
